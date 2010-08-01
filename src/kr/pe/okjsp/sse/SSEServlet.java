1c2a3efc6ba23835a17d5eebe480512de2bbc732
itismejk <itismejk@1400446e-9ab3-46cc-91c4-886daca6c3f0>
2010-08-01 03:18:52 +0000

    SSE 샘플 변경
    git-svn-id: svn://www.okjsp.pe.kr/okjsp/trunk/html5@483 1400446e-9ab3-46cc-91c4-886daca6c3f0

--- src/kr/pe/okjsp/sse/SSEServlet.java
+++ src/kr/pe/okjsp/sse/SSEServlet.java
@@ -2,42 +2,33 @@ package kr.pe.okjsp.sse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import kr.pe.okjsp.member.Member;
-import kr.pe.okjsp.util.DbCon;
+import kr.pe.okjsp.MemoDao;
 
 public class SSEServlet extends HttpServlet {
 
-  public void service(HttpServletRequest req, HttpServletResponse res)
-                        throws IOException, ServletException {
-    PrintWriter out = res.getWriter();
+    public void service(HttpServletRequest req, HttpServletResponse res)
+	    throws IOException, ServletException {
+	PrintWriter out = res.getWriter();
 
-    res.setContentType("text/event-stream");
-    
-    res.setCharacterEncoding("utf-8");
-//    res.setHeader("Cache-Control", "max-age=172800");
-//    res.setHeader("Connection", "keep-alive");
-//    res.setHeader("Transfer-Encoding", "Identity");
+	res.setContentType("text/event-stream");
+	res.setCharacterEncoding("utf-8");
 
-    res.flushBuffer();
-    
-    out.write("data: Server sends timestamp 1279969572");
-    out.write("\n");
-    out.write("\n");
-    out.flush();
-    
-    out.close();
-    
-  } // end doPost()
+	MemoDao memoDao = new MemoDao();
+	
+	out.write("retry: 5000");
+	out.write("\n");
+	out.write("\n");
+	out.write("data: " + memoDao.getMemoCount(Integer.parseInt(req.getParameter("seq"))));
+	out.flush();
+
+	out.close();
+
+    }
 
 }
